00ca6d54bf984c629b60564d2d45d0e767bb1484
Suleiman Umudov <suleiman.umudov.triare@gmail.com>
2023-02-10 04:00:03 +0200

    updated gitignore, removed commented out code

--- main/main.c
+++ main/main.c
@@ -27,16 +27,4 @@ void app_main()
     {
         ESP_LOGI(TAG, "Failed to connected with Wi-Fi, check your network Credentials\n");
     }
-
-    // while (1)
-    // {
-    //     ESP_LOGI(TAG, "Taking picture...");
-    //     camera_fb_t *pic = esp_camera_fb_get();
-
-    //     // use pic->buf to access the image
-    //     ESP_LOGI(TAG, "Picture taken! Its size was: %zu bytes", pic->len);
-    //     esp_camera_fb_return(pic);
-
-    //     vTaskDelay(5000 / portTICK_RATE_MS);
-    // }
 }
